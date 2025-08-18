4f55e3d34c6308a7f022dae3b4f78be1057bbe65
yjibus01 <yjibus01@gmail.com>
2025-08-19 02:09:59 +0900

    css 파일 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'   // ✅ 여기 App.jsx 맞는지 확인
-import './index.css'
+import './css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
