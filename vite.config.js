8bf2feb2b574308dcc2be02ddbbb05ba45ac6235
yjibus01 <yjibus01@gmail.com>
2025-08-19 18:12:44 +0900

    vite.config.js 의 base 'todo' 로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project/', // 여기에 실제 저장소 이름 입력
+  base: '/todo/', // 여기에 실제 저장소 이름 입력
   build: {
     outDir: 'dist'
   }
